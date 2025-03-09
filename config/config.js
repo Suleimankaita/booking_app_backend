20df805db8123091db4cc0cf8c7b87e50bbcda01
suleiman yusuf kaita <suleiman20015kaita@gmail.com>
2025-03-09 11:18:48 +0100

    sa

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const { options } = require("../routes/root");
 
-const list=['http://localhost:3500','http://localhost:3000'];
+const list=['http://localhost:3500','http://localhost:3000','https://booking-app-frotent.onrender.com'];
 
 const option={
     origin:(origin,callback)=>{
